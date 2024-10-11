b36afaef993d429aff7a8fcdec927c0e3d9cd2e6
tlukaszuk <tomluk@gmail.com>
2024-10-11 20:57:17 +0200

    new results and datasets info

--- cpl/consts.py
+++
@@ -1,9 +0,0 @@
-INFINITY = (1 << 20)
-ZERO = 0.00000001
-EPSILON = 0.0000000001
-
-def EQUAL_ZERO(value):
-    return abs(value) < ZERO
-
-def EQUALS_EPSILON(value1, value2):
-    return abs(value1 - value2) < EPSILON
